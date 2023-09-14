6d380b278a90bb64c56ad21df14a6f188f364162
duyminh94 <lenguyen.minh1830@gmail.com>
2023-09-14 15:43:24 +0700

    upload 14/9

---
+++ edit-user.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+include 'db_connect.php';
+
+$id = $_SESSION['id'];
+$sql = "SELECT * FROM sys_users WHERE id='$id'";
+
+$result = mysqli_query($conn, $sql);
+$user_row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+
+if (empty($user_row)) {
+    echo 'Giá trị id: $id không tồn tại. Vui Lòng Kiểm tra';
+    die();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+
+
+    <form action="" method="POST">
+        <div class="form-group">
+            <label>ID</label>
+            <input type="text" name="id"  value="<?php echo $user_row['id'] ?>">
+        </div>
+
+        <div class="form-group">
+            <label>Người Dùng</label>
+            <input type="text" name="new_user_name" value="<?php echo $user_row['user_name'] ?>">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" id="myInput" name="new_password" value="<?php echo $user_row['passwords'] ?>">
+
+        </div>
+        <input type="checkbox" onclick="myFunction()">Show Password
+        <button type="submit" name="btnSave" class="btn btn-success">Save</button>
+        <a class="btn btn-primary" href="logout.php">logout</a>
+    </form>
+    <?php
+    if(isset($_POST['btnSave'])) {
+        $user_name = $_POST['new_user_name'];
+        $password = $_POST['new_password'];
+
+        // Câu lệnh update
+        $sql = "UPDATE sys_users SET user_name = '$user_name', passwords='$password' WHERE id='$id'";
+
+        mysqli_query($conn, $sql);
+
+        mysqli_close($conn);
+
+        header('location: home.php');
+        exit();
+    } 
+    
+    ?>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+
+    <script type="text/javascript">
+        function myFunction() {
+            var x = document.getElementById("myInput");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+    </script>
+
+</body>
+
+</html>
--- home.php
+++ home.php
@@ -1,40 +1,46 @@
-<?php 
+<?php
 session_start();
 
-if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
-    
+if (isset($_SESSION['id']) && isset($_SESSION['user_name']) && isset($_SESSION['password'])) {
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="/css/style.css">
-</head>
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
 
-<body>
-   
-    <h1 class="text-center text-black">
-        Chào Mừng, <?php echo $_SESSION['user_name']; ?>
-    </h1>
-    <br />
-    <a class="btn btn-primary" href="logout.php">logout</a>
+    <body>
 
-    <!-- Jquery -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
-</body>
+        <h1 class="text-center text-black">
+            Chào Mừng, <?php echo $_SESSION['user_name']; ?>
+        </h1>
+        <h2 class="text-center text-black">
+            <?php echo $_SESSION['password']; ?>
+        </h2>
+        <br />
+        <div class="d-flex">
+            <!-- <a class="btn btn-primary" href="logout.php">logout</a> -->
+            <a class="btn btn-success ml-3" href="edit-user.php">Chỉnh Sửa</a>
+        </div>
 
-</html>
+        <!-- Jquery -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    </body>
 
-<?php 
+    </html>
+
+<?php
 } else {
     header("Location: index.php");
     exit();
